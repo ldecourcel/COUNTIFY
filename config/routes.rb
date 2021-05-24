5f7805b544305e688ef5adcb879dece38e87ee25
Luc de Courcel <lucdecourcel@icloud.com>
2021-05-24 17:43:49 +0200

    routes and controllers initialized

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :companies, only: [ :index, :new, :create, :delete,] do
+    resources :invoices, only: [ :index, :new,  :create ]
+    resources :payments, only: [ :index, :new,  :create ]
+  end
+
+  resources :payments, only: [ :update, :show ]
+  resources :invoices, only: [ :update, :show ]
 end
