c77c6aa151e769ed4fa15e2a4c1934fd9d0407bc
Luc de Courcel <lucdecourcel@icloud.com>
2021-05-25 15:26:03 +0200

    validations ok for now

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :company
   has_many :operations
+
+  validates :iban, :swift, :account_name, presence: true
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :company
   has_many :operations
+
+  validates :date, :net_amount, :issuer, :vta, :payment_method, :tax_amount, :label, :client, presence: true
+
+  has_attached :file
 end
