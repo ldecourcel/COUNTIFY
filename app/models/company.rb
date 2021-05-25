ca6d99e3cffcb78c906260ba413b8b410e7fa2b1
Luc de Courcel <lucdecourcel@icloud.com>
2021-05-25 17:42:17 +0200

    first route ok

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,5 @@ class Company < ApplicationRecord
   belongs_to :accountant
   has_many :accounts
 
-  validates :siren, :siret, :fiscal_regim, :address, :phone_number, presence: true
+  validates :name, :siren, :siret, :fiscal_regim, :address, :phone_number, presence: true
 end
