d497cf6ada1291215afa5d81bc2258091e7a91ce
Helena Badaoui <helenabadaoui@gmail.com>
2021-05-31 17:58:13 +0200

    order asc and desc on operations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+    column_downcase = column.downcase
+    # css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = (sort_direction == "asc") ? "desc" : "asc"
+    link_to title, {:sort => column_downcase, :direction => direction}
+  end
 end
