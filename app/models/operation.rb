7df415719ddf47c9859128f9b16bad8654bc1e77
Luc de Courcel <lucdecourcel@icloud.com>
2021-06-03 10:59:36 +0200

    dash accountant ok

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -4,10 +4,14 @@ class Operation < ApplicationRecord
 
   validates :date, :amount, :details, presence: true
 
+  scope :missing, -> { where(validated: false).where(invoice_id: nil) }
+  scope :to_validate, -> { where.not(invoice_id: nil).where(validated: false) }
+  scope :validated, -> { where(validated: true) }
+
   include PgSearch::Model
   pg_search_scope :global_search,
     against: [ :amount, :details, :date ],
     using: {
-      tsearch: { prefix: true } 
+      tsearch: { prefix: true }
     }
 end
