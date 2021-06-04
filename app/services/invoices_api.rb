77ac9297b80faef12ca872f492d5b8465a14b0ad
Laura Auroux <laura.auroux@live.fr>
2021-06-04 10:35:44 +0200

    montant fixed

--- app/services/invoices_api.rb
+++ app/services/invoices_api.rb
@@ -12,19 +12,19 @@ class InvoicesApi
     # raise
     @invoice.date = Date.parse(result["document"]["inference"]["pages"][0]["prediction"]["date"]["value"])
     @invoice.issuer = result["document"]["inference"]["pages"][0]["prediction"]["supplier"]["value"]
-    @invoice.total_amount = (result["document"]["inference"]["pages"][0]["prediction"]["total_incl"]["value"]*100).to_i
+    @invoice.total_amount_cents = (result["document"]["inference"]["pages"][0]["prediction"]["total_incl"]["value"]*100).to_i
 
     if @invoice.net_amount_cents != nil
-     @invoice.net_amount_cents = (result["document"]["inference"]["pages"][0]["prediction"]["total_excl"]["value"]*100).to_i
+      @invoice.net_amount_cents = (result["document"]["inference"]["pages"][0]["prediction"]["total_excl"]["value"]*100).to_i
     end
 
     # @invoice.net_amount_cents = (result["document"]["inference"]["pages"][0]["prediction"]["total_excl"]["value"]*100).to_i
 
     if result["document"]["inference"]["pages"][0]["prediction"]["taxes"] != []
-      @invoice.tax_amount = (result["document"]["inference"]["pages"][0]["prediction"]["taxes"][0]["value"]*100).to_i
+      @invoice.tax_amount_cents = (result["document"]["inference"]["pages"][0]["prediction"]["taxes"][0]["value"]*100).to_i
       @invoice.vta = result["document"]["inference"]["pages"][0]["prediction"]["taxes"][0]["rate"]
     else
-      @invoice.tax_amount = @invoice.total_amount - @invoice.net_amount unless @invoice.total_amount.nil? || @invoice.net_amount.nil?
+      @invoice.tax_amount_cents = @invoice.total_amount - @invoice.net_amount unless @invoice.total_amount.nil? || @invoice.net_amount.nil?
     end
 
     #LOGIC TO FILL THE INVOICE
